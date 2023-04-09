f22595254c7834fb25f65b1b92a1dce57762dc0f
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-04-09 01:41:48 -0400

    Update code to take csv

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,43 @@
+import java.io.File
+import java.nio.file.Files
 import java.time.Instant
-
+data class Link(val folderTitle: String, val articleTitle: String, val url: String)
 fun main(args: Array<String>) {
-    val title = "Interesting science and psychology articles to read - part 2"
     val epochSecond = Instant.now().epochSecond
-    val list = listOf(Pair("title", "url"))
-    joinToString(
-        list,
-        StringBuilder(),
-        "\n",
-        "<DT><H3 FOLDED ADD_DATE=\"$epochSecond\">$title</H3>\n<DL><p>\n",
-        "</DL><p>"
-    ) {
-        "<DT><A HREF=\"${it.second}\" ADD_DATE=\"$epochSecond\" LAST_VISIT=\"$epochSecond\" LAST_MODIFIED=\"$epochSecond\">${it.first}</A>"
-    }.also { println(it) }
+    Files.readAllLines(File("/Users/shmuel/Downloads/microsoft_edge_collections.tsv").toPath())
+        .asSequence()
+        .drop(1)
+        .map {
+            it
+                .split("\t")
+                .iterator()
+                .run {
+                    Link(
+                        next(),
+                        next(),
+                        next().let {
+                            it.substring(12, it.lastIndexOf("\"\",\"\"websiteName\"\""))
+                        }
+                    )
+                }
+        }
+        .groupBy { it.folderTitle }
+        .map { (title, list) ->
+            joinToString(
+                list,
+                StringBuilder(),
+                "\n",
+                "<DT><H3 FOLDED ADD_DATE=\"$epochSecond\">$title</H3>\n<DL><p>\n",
+                "</DL><p>"
+            ) {
+                "<DT><A HREF=\"${it.url}\" ADD_DATE=\"$epochSecond\" LAST_VISIT=\"$epochSecond\" LAST_MODIFIED=\"$epochSecond\">${it.articleTitle}</A>"
+            }
+        }
+        .joinToString("\n", "<!DOCTYPE NETSCAPE-Bookmark-file-1>\n" +
+                "\t<HTML>\n" +
+                "\t<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\">\n" +
+                "\t<Title>Bookmarks</Title>\n" +
+                "\t<H1>Bookmarks</H1>", "</HTML>").also { println(it) }
 }
 
 fun <T> joinToString(
